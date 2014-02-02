3085d73552ccf8ba2173cd64c54eec0743b0324e
DeadLox <deadlox.pro@gmail.com>
2014-02-02 15:49:16 +0100

    Suppression la fonction de Set des pseudo

--- app/controllers/Administration.java
+++ app/controllers/Administration.java
@@ -23,24 +23,4 @@ public class Administration extends Controller {
         List<Recette> recettes = Recette.findAll();
         render(recettes);
     }
-
-    public static void setPseudo(){
-        List<Utilisateur> users = Utilisateur.findAll();
-        for (Utilisateur user : users) {
-            if (user.email.equals("dead.lox@hotmail.fr")) {
-                user.pseudo = "DeadLox";
-                user.save();
-            }
-            if (user.email.equals("mlebor@hotmail.fr")) {
-                user.pseudo = "Maryse";
-                user.save();
-            }
-            if (user.email.equals("laura-boutche9@hotmail.fr")) {
-                user.pseudo = "Laura";
-                user.save();
-            }
-        }
-        flash.success("Set des Pseudo effectué");
-        index();
-    }
 }
